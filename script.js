4bb29f29e37163af185694ca7b2d955e42d9773c
Zaruhi Sahakyan <42776122+zara-sahakyan@users.noreply.github.com>
2024-05-19 18:42:01 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 /* class Author */
 class Author {
 	constructor(name, email, gender) {
@@ -34,7 +35,7 @@ class Author {
 	}
 }
 
-const author1 = new Author("John Doe", "john@example.com", "Male");
+const author1 = new Author("Hovhannes Tumanyan", "hovo@example.com", "Male");
 console.log(author1.toString());
 
 /* class Book */
@@ -80,3 +81,208 @@ class Book {
 const book1 = new Book("Book", author1, 30, 50);
 console.log(book1.toString());
 console.log(`Profit from ${book1.title}: ${book1.getProfit()}$`);
+
+/* class Account */
+class Account {
+	constructor(id, name, balance) {
+		this._id = id;
+		this._name = name;
+		this._balance = balance;
+	}
+	get id() {
+		return this._id;
+	}
+	get name() {
+		return this._name;
+	}
+	set name(name) {
+		this._name = name;
+	}
+	get balance() {
+		return this._balance;
+	}
+	set balance(balance) {
+		this._balance = balance;
+	}
+	credit(amount) {
+		this._balance += amount;
+		return this._balance;
+	}
+	debit(amount) {
+		if (amount < this._balance) {
+			this._balance -= amount;
+			return this._balance;
+		} else {
+			console.log("Amount exceeded balance.");
+		}
+	}
+	transferTo(anotherAccount, amount) {
+		if (amount < this._balance) {
+			this._balance -= amount;
+			anotherAccount.credit(amount);
+			return this._balance;
+		} else {
+			console.log("Amount exceeded balance.");
+		}
+	}
+	static identifyAccounts(accountFirst, accountSecond) {
+		return (
+			accountFirst.id === accountSecond.id &&
+			accountFirst.name === accountSecond.name &&
+			accountFirst.balance === accountSecond.balance
+		);
+	}
+	toString() {
+		return `Account /ID: ${this.id}/ Name: ${this.name}/ Balance: ${this.balance}/`;
+	}
+}
+const account1 = new Account(1, "Ani", 1000);
+const account2 = new Account(2, "Ara", 2000);
+
+console.log(account1.credit(600));
+console.log(account1.debit(100));
+console.log(account1.transferTo(account2, 500));
+console.log(account1.toString());
+console.log(Account.identifyAccounts(account1, account2));
+
+/* class Person */
+class Person {
+	constructor(firstName, lastName, gender, age) {
+		this._firstName = firstName;
+		this._lastName = lastName;
+		this._gender = gender;
+		this._age = age;
+	}
+	get firstName() {
+		return this._firstName;
+	}
+
+	set firstName(firstName) {
+		this._firstName = firstName;
+	}
+
+	get lastName() {
+		return this._lastName;
+	}
+
+	set lastName(lastName) {
+		this._lastName = lastName;
+	}
+
+	get gender() {
+		return this._gender;
+	}
+
+	set gender(gender) {
+		this._gender = gender;
+	}
+
+	get age() {
+		return this._age;
+	}
+
+	set age(age) {
+		this._age = age;
+	}
+	toString() {
+		return `Person: ${this._firstName} ${this._lastName}/ Gender: ${this._gender}/ Age: ${this._age}`;
+	}
+}
+
+const person1 = new Person("Hamo", "Sahyan", "Male", "100");
+console.log(person1.toString());
+
+/* class Student */
+class Student extends Person {
+	constructor(firstName, lastName, gender, age, program, year, fee) {
+		super(firstName, lastName, gender, age);
+		this._program = program;
+		this._year = year;
+		this._fee = fee;
+		this._exams = {}; // Object to store exam grades by program
+	}
+	get program() {
+		return this._program;
+	}
+
+	set program(program) {
+		this._program = program;
+	}
+
+	get year() {
+		return this._year;
+	}
+
+	set year(year) {
+		this._year = year;
+	}
+
+	get fee() {
+		return this._fee;
+	}
+
+	set fee(fee) {
+		this._fee = fee;
+	}
+	passExam(program, grade) {
+		this._exams[program] = grade;
+		let allPassed = true;
+		for (const exam in this._exams) {
+			if (this._exams.hasOwnProperty(exam)) {
+				if (this._exams[exam] < 50) {
+					allPassed = false;
+					break;
+				}
+			}
+		}
+		if (allPassed) {
+			this._year++;
+		}
+	}
+	toString() {
+		return `${super.toString()}/ Year: ${this._year}`;
+	}
+}
+const student1 = new Student(
+	"Poghos",
+	"Poghosyan",
+	"Male",
+	20,
+	["Math", "Physics"],
+	1,
+	1000
+);
+student1.passExam("Math", 60);
+student1.passExam("Physics", 50);
+console.log(student1.toString());
+
+/* class Staff */
+class Teacher extends Person {
+	constructor(firstName, lastName, gender, age, program, pay) {
+		super(firstName, lastName, gender, age);
+		this._program = program;
+		this._pay = pay;
+	}
+
+	get program() {
+		return this._program;
+	}
+
+	set program(program) {
+		this._program = program;
+	}
+
+	get pay() {
+		return this._pay;
+	}
+
+	set pay(pay) {
+		this._pay = pay;
+	}
+
+	toString() {
+		return `${super.toString()}/ Program: ${this._program}/ Pay: ${this._pay}`;
+	}
+}
+const teacher1 = new Teacher("Ani", "Poghosyan", "Female", 30, "Math", 150000);
+console.log(teacher1.toString());
